e297467c76a3efb5d9a08154f03879b1e33a002c
Sebastien Ros <sebastien.ros@microsoft.com>
2014-05-03 17:38:24 -0700

    Removing evil tabs

--- src/Microsoft.AspNet.Security/Authorization/AuthorizationPolicy.cs
+++ src/Microsoft.AspNet.Security/Authorization/AuthorizationPolicy.cs
@@ -6,26 +6,26 @@ using System.Threading.Tasks;
 
 namespace Microsoft.AspNet.Security.Authorization
 {
-	/// <summary>
-	/// This class provides a base implementation for <see cref="IAuthorizationPolicy" />
-	/// </summary>
-	public abstract class AuthorizationPolicy : IAuthorizationPolicy
-	{
+    /// <summary>
+    /// This class provides a base implementation for <see cref="IAuthorizationPolicy" />
+    /// </summary>
+    public abstract class AuthorizationPolicy : IAuthorizationPolicy
+    {
         public int Order { get; set; }
         
         public virtual async Task ApplyingAsync(AuthorizationPolicyContext context)
         {
-    		await Task.FromResult(0);
+            await Task.FromResult(0);
         }
 
         public virtual async Task ApplyAsync(AuthorizationPolicyContext context) 
         {
-    		await Task.FromResult(0);
+            await Task.FromResult(0);
         }
 
         public virtual async Task AppliedAsync(AuthorizationPolicyContext context)
         {
-        	await Task.FromResult(0);
+            await Task.FromResult(0);
         }
     }
 }
