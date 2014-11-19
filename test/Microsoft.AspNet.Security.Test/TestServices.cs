fb5db4dcf539a37f3c7d9ad30c9adac5e10c9827
Hao Kung <haok@microsoft.com>
2014-11-19 02:54:07 -0800

    Fix security tests with IServiceManifest

--- test/Microsoft.AspNet.Security.Test/TestServices.cs
+++ test/Microsoft.AspNet.Security.Test/TestServices.cs
@@ -2,9 +2,12 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
+using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Security.DataProtection;
 using Microsoft.Framework.DependencyInjection;
 using Microsoft.Framework.DependencyInjection.Fallback;
+using Microsoft.Framework.DependencyInjection.ServiceLookup;
 using Microsoft.Framework.Runtime;
 
 namespace Microsoft.AspNet.Security
@@ -16,7 +19,20 @@ namespace Microsoft.AspNet.Security
             var collection = new ServiceCollection();
             collection.AddSingleton<IApplicationEnvironment, TestApplicationEnvironment>();
             collection.Add(DataProtectionServices.GetDefaultServices());
+            collection.AddSingleton<IServiceManifest, TestManifest>();
             return collection.BuildServiceProvider();
         }
+
+        private class TestManifest : IServiceManifest
+        {
+            public IEnumerable<Type> Services
+            {
+                get
+                {
+                    return new Type[] { typeof(IApplicationEnvironment), typeof(IDataProtectionProvider) };
+                }
+            }
+        }
+
     }
 }
