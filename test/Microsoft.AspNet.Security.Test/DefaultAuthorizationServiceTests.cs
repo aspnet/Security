c3f2835ab979c730aaaaccfdff32dfe96a75ff9a
Hao Kung <haok@microsoft.com>
2015-01-26 16:55:01 -0800

    Add more tests

--- test/Microsoft.AspNet.Security.Test/DefaultAuthorizationServiceTests.cs
+++ test/Microsoft.AspNet.Security.Test/DefaultAuthorizationServiceTests.cs
@@ -344,6 +344,40 @@ namespace Microsoft.AspNet.Security.Test
         }
 
         [Fact]
+        public async Task Authorize_PolicyWillFilterAuthenticationType()
+        {
+            // Arrange
+            var policy = new AuthorizationPolicyBuilder("Bogus").RequiresClaim(ClaimTypes.Name);
+            var authorizationService = BuildAuthorizationService();
+            var user = new ClaimsPrincipal(
+                new ClaimsIdentity(new Claim[] { new Claim(ClaimTypes.Name, "Name") }, "AuthType")
+                );
+
+            // Act
+            var allowed = await authorizationService.AuthorizeAsync(user, null, policy.Build());
+
+            // Assert
+            Assert.False(allowed);
+        }
+
+        [Fact]
+        public async Task Authorize_PolicyCanFilterMultipleAuthenticationType()
+        {
+            // Arrange
+            var policy = new AuthorizationPolicyBuilder("One", "Two").RequiresClaim(ClaimTypes.Name, "one").RequiresClaim(ClaimTypes.Name, "two");
+            var authorizationService = BuildAuthorizationService();
+            var user = new ClaimsPrincipal();
+            user.AddIdentity(new ClaimsIdentity(new Claim[] { new Claim(ClaimTypes.Name, "one") }, "One"));
+            user.AddIdentity(new ClaimsIdentity(new Claim[] { new Claim(ClaimTypes.Name, "two") }, "Two"));
+
+            // Act
+            var allowed = await authorizationService.AuthorizeAsync(user, null, policy.Build());
+
+            // Assert
+            Assert.True(allowed);
+        }
+
+        [Fact]
         public async Task RolePolicyCanRequireSingleRole()
         {
             // Arrange
