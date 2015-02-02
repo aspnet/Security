fe55d69c91f1f8eb742df33bf4910224455f4599
Hao Kung <haok@microsoft.com>
2015-02-02 13:13:50 -0800

    Remove usings

--- test/Microsoft.AspNet.Security.Test/AuthorizationPolicyFacts.cs
+++ test/Microsoft.AspNet.Security.Test/AuthorizationPolicyFacts.cs
@@ -1,13 +1,7 @@
 // Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using Microsoft.Framework.DependencyInjection;
-using Microsoft.Framework.DependencyInjection.Fallback;
 using Xunit;
 
 namespace Microsoft.AspNet.Security.Test
