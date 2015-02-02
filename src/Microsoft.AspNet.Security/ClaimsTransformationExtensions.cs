8d6244d5e51ae4c03c823128d57167f41e70b122
Hao Kung <haok@microsoft.com>
2015-02-02 13:55:08 -0800

    Cleanup

--- src/Microsoft.AspNet.Security/ClaimsTransformationExtensions.cs
+++ src/Microsoft.AspNet.Security/ClaimsTransformationExtensions.cs
@@ -1,10 +1,9 @@
 // Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using Microsoft.Framework.DependencyInjection;
-using Microsoft.Framework.OptionsModel;
 using System;
 using Microsoft.AspNet.Security;
+using Microsoft.Framework.DependencyInjection;
 
 namespace Microsoft.AspNet.Builder
 {
@@ -19,11 +18,10 @@ namespace Microsoft.AspNet.Builder
         }
 
         /// <summary>
-        /// Adds a cookie-based authentication middleware to your web application pipeline.
+        /// Adds a claims transformation middleware to your web application pipeline.
         /// </summary>
         /// <param name="app">The IApplicationBuilder passed to your configuration method</param>
         /// <param name="configureOptions">Used to configure the options for the middleware</param>
-        /// <param name="optionsName">The name of the options class that controls the middleware behavior, null will use the default options</param>
         /// <returns>The original app parameter</returns>
         public static IApplicationBuilder UseClaimsTransformation([NotNull] this IApplicationBuilder app, Action<ClaimsTransformationOptions> configureOptions = null)
         {
