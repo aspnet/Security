c3db709df6bf73670072054217303c095e063195
Hao Kung <haok@microsoft.com>
2015-02-17 13:04:04 -0800

    Move ServiceCollection extension methods
    Fixes https://github.com/aspnet/Security/issues/107
    Fixes https://github.com/aspnet/Security/issues/113

--- src/Microsoft.AspNet.Security.OAuth/OAuthAuthenticationExtensions.cs
+++ src/Microsoft.AspNet.Security.OAuth/OAuthAuthenticationExtensions.cs
@@ -2,9 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using System.Globalization;
 using Microsoft.AspNet.Security.OAuth;
-using Microsoft.AspNet.Security.Infrastructure;
 using Microsoft.Framework.OptionsModel;
 
 namespace Microsoft.AspNet.Builder
