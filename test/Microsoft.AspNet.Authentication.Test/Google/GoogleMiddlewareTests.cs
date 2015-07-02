9bdb8b5c4a58cb63222f64e6dc2cafb0900673bb
Hao Kung <haok@microsoft.com>
2015-07-01 19:00:40 -0700

    Nuke code

--- test/Microsoft.AspNet.Authentication.Test/Google/GoogleMiddlewareTests.cs
+++ test/Microsoft.AspNet.Authentication.Test/Google/GoogleMiddlewareTests.cs
@@ -615,31 +615,5 @@ namespace Microsoft.AspNet.Authentication.Google
                 });
             });
         }
-
-        private class CustomStateDataFormat : ISecureDataFormat<AuthenticationProperties>
-        {
-            private static string _lastSavedAuthenticationProperties;
-
-            public string Protect(AuthenticationProperties data)
-            {
-                _lastSavedAuthenticationProperties = Serialize(data);
-                return "ValidStateData";
-            }
-
-            public AuthenticationProperties Unprotect(string state)
-            {
-                return state == "ValidStateData" ? DeSerialize(_lastSavedAuthenticationProperties) : null;
-            }
-
-            private string Serialize(AuthenticationProperties data)
-            {
-                return JsonConvert.SerializeObject(data, Formatting.Indented);
-            }
-
-            private AuthenticationProperties DeSerialize(string state)
-            {
-                return JsonConvert.DeserializeObject<AuthenticationProperties>(state);
-            }
-        }
     }
 }
