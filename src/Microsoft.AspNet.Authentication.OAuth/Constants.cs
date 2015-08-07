b861bc84ece100bfa178c1c226649a073d173413
Chris R <Tratcher@outlook.com>
2015-08-07 11:30:41 -0700

    Remove unused code.

--- src/Microsoft.AspNet.Authentication.OAuth/Constants.cs
+++ src/Microsoft.AspNet.Authentication.OAuth/Constants.cs
@@ -1,12 +1,10 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-
 namespace Microsoft.AspNet.Authentication.OAuth
 {
     internal static class Constants
     {
-        internal const string SecurityAuthenticate = "security.Authenticate";
         internal const string CorrelationPrefix = ".AspNet.Correlation.";
     }
 }
