0b5dc1e2453fa9b0235b8a4718a3f550d6dc5896
Hao Kung <haok@microsoft.com>
2016-01-14 17:43:25 -0800

    Delegate => AssertionRequirement sugar
    Tweaks RequireDelegate a bit to make it simplier:
    RequireAssertion(authContext => bool) is used to register a handler
    directly without any of the mechanical methods/classes.
    
    Intended to make it pretty easy to do simple validations against a
    ClaimsPrincipal

---
+++ src/Microsoft.AspNet.Authorization/Infrastructure/AssertionRequirement.cs
@@ -0,0 +1,28 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+
+namespace Microsoft.AspNet.Authorization.Infrastructure
+{
+    public class AssertionRequirement : AuthorizationHandler<AssertionRequirement>, IAuthorizationRequirement
+    {
+        /// <summary>
+        /// Function that is called to handle this requirement
+        /// </summary>
+        public Func<AuthorizationContext, bool> Handler { get; }
+
+        public AssertionRequirement(Func<AuthorizationContext, bool> handleMe)
+        {
+            Handler = handleMe;
+        }
+
+        protected override void Handle(AuthorizationContext context, AssertionRequirement requirement)
+        {
+            if (Handler(context))
+            {
+                context.Succeed(requirement);
+            }
+        }
+    }
+}
