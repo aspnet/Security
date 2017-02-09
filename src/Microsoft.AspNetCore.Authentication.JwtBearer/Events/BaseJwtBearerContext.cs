5a99f5bf1f6d12a6282be1cffc5bdace4ef78e32
Hao Kung <haok@microsoft.com>
2017-02-09 13:32:26 -0800

    Port jwtbearer

--- src/Microsoft.AspNetCore.Authentication.JwtBearer/Events/BaseJwtBearerContext.cs
+++ src/Microsoft.AspNetCore.Authentication.JwtBearer/Events/BaseJwtBearerContext.cs
@@ -2,24 +2,21 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 
 namespace Microsoft.AspNetCore.Authentication.JwtBearer
 {
     public class BaseJwtBearerContext : BaseControlContext
     {
-        public BaseJwtBearerContext(HttpContext context, JwtBearerOptions options)
+        public BaseJwtBearerContext(HttpContext context, AuthenticationScheme scheme, JwtBearerOptions options)
             : base(context)
         {
-            if (options == null)
-            {
-                throw new ArgumentNullException(nameof(options));
-            }
-
-            Options = options;
+            Options = options ?? throw new ArgumentNullException(nameof(options));
+            Scheme = scheme ?? throw new ArgumentException(nameof(scheme));
         }
 
         public JwtBearerOptions Options { get; }
+
+        public AuthenticationScheme Scheme { get; }
     }
 }
