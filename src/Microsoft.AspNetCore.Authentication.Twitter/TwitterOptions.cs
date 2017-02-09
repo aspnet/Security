8448bf0e9e745030e8e4c9f6bdccab50caffa15b
Hao Kung <haok@microsoft.com>
2017-02-09 13:00:02 -0800

    Migrate twitter to new stack

--- src/Microsoft.AspNetCore.Authentication.Twitter/TwitterOptions.cs
+++ src/Microsoft.AspNetCore.Authentication.Twitter/TwitterOptions.cs
@@ -6,9 +6,10 @@ using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.OAuth.Claims;
 using Microsoft.AspNetCore.Authentication.Twitter;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Http;
 
-namespace Microsoft.AspNetCore.Builder
+namespace Microsoft.AspNetCore.Authentication.Twitter
 {
     /// <summary>
     /// Options for the Twitter authentication middleware.
@@ -20,8 +21,6 @@ namespace Microsoft.AspNetCore.Builder
         /// </summary>
         public TwitterOptions()
         {
-            AuthenticationScheme = TwitterDefaults.AuthenticationScheme;
-            DisplayName = AuthenticationScheme;
             CallbackPath = new PathString("/signin-twitter");
             BackchannelTimeout = TimeSpan.FromSeconds(60);
             Events = new TwitterEvents();
@@ -58,13 +57,14 @@ namespace Microsoft.AspNetCore.Builder
         /// Gets or sets the type used to secure data handled by the middleware.
         /// </summary>
         public ISecureDataFormat<RequestToken> StateDataFormat { get; set; }
+        public IDataProtectionProvider DataProtectionProvider { get; internal set; }
 
         /// <summary>
-        /// Gets or sets the <see cref="ITwitterEvents"/> used to handle authentication events.
+        /// Gets or sets the <see cref="TwitterEvents"/> used to handle authentication events.
         /// </summary>
-        public new ITwitterEvents Events
+        public new TwitterEvents Events
         {
-            get { return (ITwitterEvents)base.Events; }
+            get { return (TwitterEvents)base.Events; }
             set { base.Events = value; }
         }
     }
