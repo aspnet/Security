5ea25a22b4ad501fd1aea7b5aed872bc796e2e01
John Luo <jt.luo@mail.utoronto.ca>
2017-02-16 15:21:48 -0800

    Disable warning

--- samples/SocialSample/Startup.cs
+++ samples/SocialSample/Startup.cs
@@ -34,7 +34,9 @@ namespace SocialSample
             if (env.IsDevelopment())
             {
                 // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
+#pragma warning disable CS0618
                 builder.AddUserSecrets();
+#pragma warning restore CS0618
             }
 
             builder.AddEnvironmentVariables();
