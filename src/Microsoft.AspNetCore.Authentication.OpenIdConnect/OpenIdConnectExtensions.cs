b8581a8d610f4aaca484f94cc6ca5abdc799095e
Hao Kung <haok@microsoft.com>
2017-05-30 19:32:51 -0700

    Add OIDC overload

--- src/Microsoft.AspNetCore.Authentication.OpenIdConnect/OpenIdConnectExtensions.cs
+++ src/Microsoft.AspNetCore.Authentication.OpenIdConnect/OpenIdConnectExtensions.cs
@@ -19,8 +19,16 @@ namespace Microsoft.Extensions.DependencyInjection
 
         public static IServiceCollection AddOpenIdConnectAuthentication(this IServiceCollection services, string authenticationScheme, Action<OpenIdConnectOptions> configureOptions)
         {
+            services.TryAddEnumerable(ServiceDescriptor.Singleton<IInitializeOptions<OpenIdConnectOptions>, OpenIdConnectInitializer<OpenIdConnectOptions>>());
             services.AddSingleton<ConfigureDefaultOptions<OpenIdConnectOptions>, OpenIdConnectConfigureOptions>();
             return services.AddRemoteScheme<OpenIdConnectOptions, OpenIdConnectHandler>(authenticationScheme, authenticationScheme, configureOptions);
+        }
+
+        public static IServiceCollection AddOpenIdConnectAuthentication<TOptions>(this IServiceCollection services, string authenticationScheme, Action<TOptions> configureOptions)
+            where TOptions : OpenIdConnectOptions, new()
+        {
+            services.TryAddEnumerable(ServiceDescriptor.Singleton<IInitializeOptions<TOptions>, OpenIdConnectInitializer<TOptions>>());
+            return services.AddRemoteScheme<TOptions, OpenIdConnectHandler<TOptions>>(authenticationScheme, authenticationScheme, configureOptions);
         }
     }
 }
