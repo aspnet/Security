1a8eed6c9d2711ab27ae38b48d270c8262ef2e77
Hao Kung <haok@microsoft.com>
2017-11-27 12:33:08 -0800

    Fix sign in handlers as well

--- src/Microsoft.AspNetCore.Authentication/SignOutAuthenticationHandler.cs
+++ src/Microsoft.AspNetCore.Authentication/SignOutAuthenticationHandler.cs
@@ -1,6 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
@@ -14,15 +15,30 @@ namespace Microsoft.AspNetCore.Authentication
     public abstract class SignOutAuthenticationHandler<TOptions> : AuthenticationHandler<TOptions>, IAuthenticationSignOutHandler
         where TOptions : AuthenticationSchemeOptions, new()
     {
+        private bool _inSignOut;
+
         public SignOutAuthenticationHandler(IOptionsMonitor<TOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock) : base(options, logger, encoder, clock)
         { }
 
         public Task SignOutAsync(AuthenticationProperties properties)
         {
-            var target = ResolveTarget(Options.ForwardSignOut);
-            return (target != null)
-                ? Context.SignOutAsync(target, properties)
-                : HandleSignOutAsync(properties ?? new AuthenticationProperties());
+            if (_inSignOut)
+            {
+                throw new InvalidOperationException("SignOut for scheme:[" + Scheme.Name + "] resulted in a recursive call back to itself.");
+            }
+
+            _inSignOut = true;
+            try
+            {
+                var target = ResolveTarget(Options.ForwardSignOut);
+                return (target != null)
+                    ? Context.SignOutAsync(target, properties)
+                    : HandleSignOutAsync(properties ?? new AuthenticationProperties());
+            }
+            finally
+            {
+                _inSignOut = false;
+            }
         }
 
         /// <summary>
