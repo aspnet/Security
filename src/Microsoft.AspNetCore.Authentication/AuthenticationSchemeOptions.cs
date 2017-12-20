efb02a47c8c61d0b51576d6abcdf3680804db058
Hao Kung <haok@microsoft.com>
2017-12-20 15:39:33 -0800

    Document the Forward options better

--- src/Microsoft.AspNetCore.Authentication/AuthenticationSchemeOptions.cs
+++ src/Microsoft.AspNetCore.Authentication/AuthenticationSchemeOptions.cs
@@ -38,18 +38,49 @@ namespace Microsoft.AspNetCore.Authentication
         /// </summary>
         public Type EventsType { get; set; }
 
-        // Scheme forwarding properties
-
+        /// <summary>
+        /// If set, this specifies a default scheme that authentication handlers should forward all authentication operations to
+        /// by default. The default forwarding logic will check the most specific ForwardAuthenticate/Challenge/Forbid/SignIn/SignOut 
+        /// setting first, followed by checking the ForwardDefaultSelector, followed by ForwardDefault. The first non null result
+        /// will be used as the target scheme to forward to.
+        /// </summary>
         public string ForwardDefault { get; set; }
 
+        /// <summary>
+        /// If set, this specifies the target scheme that this scheme should forward AuthenticateAsync calls to.
+        /// For example Context.AuthenticateAsync("ThisScheme") => Context.AuthenticateAsync("ForwardAuthenticateValue");
+        /// </summary>
         public string ForwardAuthenticate { get; set; }
+
+        /// <summary>
+        /// If set, this specifies the target scheme that this scheme should forward ChallengeAsync calls to.
+        /// For example Context.ChallengeAsync("ThisScheme") => Context.ChallengeAsync("ForwardChallengeValue");
+        /// </summary>
         public string ForwardChallenge { get; set; }
+
+        /// <summary>
+        /// If set, this specifies the target scheme that this scheme should forward ForbidAsync calls to.
+        /// For example Context.ForbidAsync("ThisScheme") => Context.ForbidAsync("ForwardForbidValue");
+        /// </summary>
         public string ForwardForbid { get; set; }
+
+        /// <summary>
+        /// If set, this specifies the target scheme that this scheme should forward SignInAsync calls to.
+        /// For example Context.SignInAsync("ThisScheme") => Context.SignInAsync("ForwardSignInValue");
+        /// </summary>
         public string ForwardSignIn { get; set; }
+
+        /// <summary>
+        /// If set, this specifies the target scheme that this scheme should forward SignOutAsync calls to.
+        /// For example Context.SignOutAsync("ThisScheme") => Context.SignInAsync("ForwardSignOutValue");
+        /// </summary>
         public string ForwardSignOut { get; set; }
 
         /// <summary>
-        /// Used to select a default scheme to target based on the request.
+        /// Used to select a default scheme from the current request that authentication handlers should forward all authentication operations to
+        /// by default. The default forwarding logic will check the most specific ForwardAuthenticate/Challenge/Forbid/SignIn/SignOut 
+        /// setting first, followed by checking the ForwardDefaultSelector, followed by ForwardDefault. The first non null result
+        /// will be used as the target scheme to forward to.
         /// </summary>
         public Func<HttpContext, string> ForwardDefaultSelector { get; set; }
 
