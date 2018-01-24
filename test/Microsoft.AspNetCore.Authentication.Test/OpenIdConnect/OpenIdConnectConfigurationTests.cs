42a4e04699b6af30bbf967a9c2c23d4758d159f4
Hao Kung <haok@microsoft.com>
2018-01-24 14:56:16 -0800

    Cleanup tests

--- test/Microsoft.AspNetCore.Authentication.Test/OpenIdConnect/OpenIdConnectConfigurationTests.cs
+++ test/Microsoft.AspNetCore.Authentication.Test/OpenIdConnect/OpenIdConnectConfigurationTests.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.Net;
-using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
