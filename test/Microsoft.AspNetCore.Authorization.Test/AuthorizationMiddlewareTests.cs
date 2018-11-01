46964d304edf7bd975f6a273dede80b7a6d3c407
James Newton-King <james@newtonking.com>
2018-11-01 22:04:24 +1300

    UseAuthorization test

--- test/Microsoft.AspNetCore.Authorization.Test/AuthorizationMiddlewareTests.cs
+++ test/Microsoft.AspNetCore.Authorization.Test/AuthorizationMiddlewareTests.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.AspNetCore.Authorization.Test.TestObjects;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.Extensions.DependencyInjection;
@@ -390,52 +391,6 @@ namespace Microsoft.AspNetCore.Authorization.Test
             }
 
             return httpContext;
-        }
-
-        private class TestAuthenticationService : IAuthenticationService
-        {
-            public bool ChallengeCalled => ChallengeCount > 0;
-            public bool ForbidCalled => ForbidCount > 0;
-            public bool AuthenticateCalled => AuthenticateCount > 0;
-
-            public int ChallengeCount { get; private set; }
-            public int ForbidCount { get; private set; }
-            public int AuthenticateCount { get; private set; }
-
-            public Task<AuthenticateResult> AuthenticateAsync(HttpContext context, string scheme)
-            {
-                AuthenticateCount++;
-
-                var identity = context.User.Identities.SingleOrDefault(i => i.AuthenticationType == scheme);
-                if (identity != null)
-                {
-                    return Task.FromResult(AuthenticateResult.Success(new AuthenticationTicket(new ClaimsPrincipal(identity), scheme)));
-                }
-
-                return Task.FromResult(AuthenticateResult.Fail("Denied"));
-            }
-
-            public Task ChallengeAsync(HttpContext context, string scheme, AuthenticationProperties properties)
-            {
-                ChallengeCount++;
-                return Task.CompletedTask;
-            }
-
-            public Task ForbidAsync(HttpContext context, string scheme, AuthenticationProperties properties)
-            {
-                ForbidCount++;
-                return Task.CompletedTask;
-            }
-
-            public Task SignInAsync(HttpContext context, string scheme, ClaimsPrincipal principal, AuthenticationProperties properties)
-            {
-                throw new NotImplementedException();
-            }
-
-            public Task SignOutAsync(HttpContext context, string scheme, AuthenticationProperties properties)
-            {
-                throw new NotImplementedException();
-            }
         }
 
         private class TestRequestDelegate
